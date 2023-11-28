d25786232294a473e2371ceac1569a360b48f2b9
aisara21 <119093375+aisara21@users.noreply.github.com>
2023-11-28 20:52:42 +0600

    test

--- shop/tests.py
+++ shop/tests.py
@@ -1,4 +1,30 @@
-from django.test import TestCase
-class Test(TestCase):
-    def test_1(self):
-        self.assertEqual(1, 1)
+import unittest
+from unittest.mock import MagicMock
+from .management.commands.run_telegram_bot import start, food, help_command, add_food
+
+
+class TestBot(unittest.TestCase):
+
+    def test_start(self):
+        message = MagicMock()
+        message.chat.id = 623917543
+        start(message)
+
+    def test_help_command(self):
+        message = MagicMock()
+        message.chat.id = 623917543
+        help_command(message)
+
+    def test_food(self):
+        message = MagicMock()
+        message.chat.id = 623917543
+        food(message)
+
+    def test_add_food_command(self):
+        message = MagicMock()
+        message.chat.id = 623917543
+        add_food(message)
+
+
+if __name__ == '__main__':
+    unittest.main()
